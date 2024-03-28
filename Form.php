66f5f0fe9bd38f0e8ba96d2375712cf3d3408c6d
caleb5344 <itscalebodongo@gmail.com.>
2024-03-28 18:29:11 +0300

    first commit

---
+++ Form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water/css@2/out/water.css">
+    <title>Generating QR code with PHP</title>
+</head>
+
+<body>
+    <h1>Generating QR Codes With PHP</h1>
+    <form method="post" action="generate.php">
+
+        <label for="text">Text</label><br>
+        <textarea name="text" id="text"></textarea><br>
+
+        <button>Generate</button>
+    </form>
+</body>
+
+</html>
